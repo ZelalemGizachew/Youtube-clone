7f051ff442d9761b8d7b5f2932090a2b72b18c3e
zelalem <zelalemgizachew890@gmail.com>
2023-01-16 02:54:14 +0300

    Routes for Search and Home added! Woring on SearchPage Component. Sleep time ;).

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import RecommendedVideos from "./components/RecommendedVideos/RecommendedVideos";
+import SearchPage from "./components/SearchPage/SearchPage.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="App">
-			<Header />
-			<div className="app__page">
-				<Sidebar />
-				<RecommendedVideos />
-			</div>
+			<Router>
+				<Header />
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<div className="app__page">
+								<Sidebar />
+								<RecommendedVideos />
+							</div>
+						}
+					/>
+					<Route
+						path="/search/:searchText"
+						element={
+							<div className="app__page">
+								<Sidebar />
+								<SearchPage />
+							</div>
+						}
+					/>
+				</Routes>
+			</Router>
 		</div>
 	);
 }
--- src/components/RecommendedVideos/RecommendedVideos.js
+++ src/components/RecommendedVideos/RecommendedVideos.js
@@ -15,10 +15,62 @@ function RecommendedVideos() {
 					views="1.1M views"
 					timestamp="3 years ago"
 				/>
-				<Video />
-				<Video />
-				<Video />
-				<Video />
+				<Video
+					image="https://i.ytimg.com/vi/Ap6LuRkg7rk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAZ2cR1BEstUKdK1IERMjrXm2jEZw"
+					title="Ethiopia | እጅጋየሁ ሽባባው(ጂጂ) - ካህኔ | Egigayehu Shibabaw (Gigi) - Kahne  Design by Nik Design"
+					channelImage="https://yt3.ggpht.com/ytc/AMLnZu8oGRKJmhYACbytqwO0zsL8UsUeyBWgfzOufjNN=s68-c-k-c0x00ffffff-no-rj"
+					channel="NIK Graphics"
+					views="1.1M views"
+					timestamp="3 years ago"
+				/>
+				<Video
+					image="https://i.ytimg.com/vi/Ap6LuRkg7rk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAZ2cR1BEstUKdK1IERMjrXm2jEZw"
+					title="Ethiopia | እጅጋየሁ ሽባባው(ጂጂ) - ካህኔ | Egigayehu Shibabaw (Gigi) - Kahne  Design by Nik Design"
+					channelImage="https://yt3.ggpht.com/ytc/AMLnZu8oGRKJmhYACbytqwO0zsL8UsUeyBWgfzOufjNN=s68-c-k-c0x00ffffff-no-rj"
+					channel="NIK Graphics"
+					views="1.1M views"
+					timestamp="3 years ago"
+				/>
+				<Video
+					image="https://i.ytimg.com/vi/Ap6LuRkg7rk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAZ2cR1BEstUKdK1IERMjrXm2jEZw"
+					title="Ethiopia | እጅጋየሁ ሽባባው(ጂጂ) - ካህኔ | Egigayehu Shibabaw (Gigi) - Kahne  Design by Nik Design"
+					channelImage="https://yt3.ggpht.com/ytc/AMLnZu8oGRKJmhYACbytqwO0zsL8UsUeyBWgfzOufjNN=s68-c-k-c0x00ffffff-no-rj"
+					channel="NIK Graphics"
+					views="1.1M views"
+					timestamp="3 years ago"
+				/>
+				<Video
+					image="https://i.ytimg.com/vi/Ap6LuRkg7rk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAZ2cR1BEstUKdK1IERMjrXm2jEZw"
+					title="Ethiopia | እጅጋየሁ ሽባባው(ጂጂ) - ካህኔ | Egigayehu Shibabaw (Gigi) - Kahne  Design by Nik Design"
+					channelImage="https://yt3.ggpht.com/ytc/AMLnZu8oGRKJmhYACbytqwO0zsL8UsUeyBWgfzOufjNN=s68-c-k-c0x00ffffff-no-rj"
+					channel="NIK Graphics"
+					views="1.1M views"
+					timestamp="3 years ago"
+				/>
+				<Video
+					image="https://i.ytimg.com/vi/Ap6LuRkg7rk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAZ2cR1BEstUKdK1IERMjrXm2jEZw"
+					title="Ethiopia | እጅጋየሁ ሽባባው(ጂጂ) - ካህኔ | Egigayehu Shibabaw (Gigi) - Kahne  Design by Nik Design"
+					channelImage="https://yt3.ggpht.com/ytc/AMLnZu8oGRKJmhYACbytqwO0zsL8UsUeyBWgfzOufjNN=s68-c-k-c0x00ffffff-no-rj"
+					channel="NIK Graphics"
+					views="1.1M views"
+					timestamp="3 years ago"
+				/>
+				<Video
+					image="https://i.ytimg.com/vi/Ap6LuRkg7rk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAZ2cR1BEstUKdK1IERMjrXm2jEZw"
+					title="Ethiopia | እጅጋየሁ ሽባባው(ጂጂ) - ካህኔ | Egigayehu Shibabaw (Gigi) - Kahne  Design by Nik Design"
+					channelImage="https://yt3.ggpht.com/ytc/AMLnZu8oGRKJmhYACbytqwO0zsL8UsUeyBWgfzOufjNN=s68-c-k-c0x00ffffff-no-rj"
+					channel="NIK Graphics"
+					views="1.1M views"
+					timestamp="3 years ago"
+				/>
+				<Video
+					image="https://i.ytimg.com/vi/Ap6LuRkg7rk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAZ2cR1BEstUKdK1IERMjrXm2jEZw"
+					title="Ethiopia | እጅጋየሁ ሽባባው(ጂጂ) - ካህኔ | Egigayehu Shibabaw (Gigi) - Kahne  Design by Nik Design"
+					channelImage="https://yt3.ggpht.com/ytc/AMLnZu8oGRKJmhYACbytqwO0zsL8UsUeyBWgfzOufjNN=s68-c-k-c0x00ffffff-no-rj"
+					channel="NIK Graphics"
+					views="1.1M views"
+					timestamp="3 years ago"
+				/>
 			</div>
 		</div>
 	);
